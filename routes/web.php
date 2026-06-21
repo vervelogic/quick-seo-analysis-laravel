8107b4e6599c4074651af64f6b331b5daf2d4a71
vervelogic <abhishek@vervelogic.com>
2026-06-21 09:45:57 +0530

    Harden public route throttling

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\ScanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('home.index'))->name('home');
-Route::post('/scan', ScanController::class)->name('scan.store');
+Route::post('/scan', ScanController::class)->middleware('throttle:scan')->name('scan.store');
 Route::get('/report/{scan:uuid}', ReportController::class)->name('report.show');
-Route::post('/lead-capture', LeadCaptureController::class)->name('lead.capture');
+Route::post('/lead-capture', LeadCaptureController::class)->middleware('throttle:lead-capture')->name('lead.capture');
