b359f72dc56570b59eaadc815f6120b75c5e8581
vervelogic <abhishek@vervelogic.com>
2026-06-24 18:55:41 +0530

    Register legacy import artisan commands

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Console\Commands\ImportLegacyDotnetCommand;
+use App\Console\Commands\InspectLegacyDotnetCommand;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('qsa:about', function () {
     $this->info('Quick SEO Analysis v1');
 })->purpose('Show Quick SEO Analysis project information');
+
+Artisan::resolve(InspectLegacyDotnetCommand::class);
+Artisan::resolve(ImportLegacyDotnetCommand::class);
