71bfa5ab8054ef118606504709703ccc74a4bf3b
vervelogic <abhishek@vervelogic.com>
2026-06-24 18:50:32 +0530

    Expose legacy company metadata fields

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -16,11 +16,16 @@ class Company extends Model
         'domain',
         'brand_settings',
         'settings',
+        'legacy_company_logo',
+        'legacy_pdf_logo',
+        'legacy_company_description',
+        'legacy_metadata',
     ];
 
     protected $casts = [
         'brand_settings' => 'array',
         'settings' => 'array',
+        'legacy_metadata' => 'array',
     ];
 
     public function users(): HasMany
