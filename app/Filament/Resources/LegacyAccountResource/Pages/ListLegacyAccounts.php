609f0bfcbf95836f1c27274b8bdeafd3684109eb
vervelogic <abhishek@vervelogic.com>
2026-06-26 16:21:28 +0530

    Add legacy account list page

---
+++ app/Filament/Resources/LegacyAccountResource/Pages/ListLegacyAccounts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\LegacyAccountResource\Pages;
+
+use App\Filament\Resources\LegacyAccountResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListLegacyAccounts extends ListRecords
+{
+    protected static string $resource = LegacyAccountResource::class;
+}
