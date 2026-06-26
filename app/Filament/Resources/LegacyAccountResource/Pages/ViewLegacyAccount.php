598ab4ba6cd4ff5fed4ca236dc351afdccb80836
vervelogic <abhishek@vervelogic.com>
2026-06-26 16:21:48 +0530

    Add legacy account view page

---
+++ app/Filament/Resources/LegacyAccountResource/Pages/ViewLegacyAccount.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\LegacyAccountResource\Pages;
+
+use App\Filament\Resources\LegacyAccountResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewLegacyAccount extends ViewRecord
+{
+    protected static string $resource = LegacyAccountResource::class;
+}
