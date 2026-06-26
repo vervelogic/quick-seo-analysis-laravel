0148230439790eca05e405b89462e878dc78572d
vervelogic <abhishek@vervelogic.com>
2026-06-26 07:42:57 +0530

    Add report usage create page

---
+++ app/Filament/Resources/ReportUsageResource/Pages/CreateReportUsage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ReportUsageResource\Pages;
+
+use App\Filament\Resources\ReportUsageResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateReportUsage extends CreateRecord
+{
+    protected static string $resource = ReportUsageResource::class;
+}
