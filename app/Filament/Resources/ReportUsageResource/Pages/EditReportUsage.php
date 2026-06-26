d5f5361613d19ab861187a7d131b00171b71a477
vervelogic <abhishek@vervelogic.com>
2026-06-26 07:43:06 +0530

    Add report usage edit page

---
+++ app/Filament/Resources/ReportUsageResource/Pages/EditReportUsage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ReportUsageResource\Pages;
+
+use App\Filament\Resources\ReportUsageResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditReportUsage extends EditRecord
+{
+    protected static string $resource = ReportUsageResource::class;
+}
