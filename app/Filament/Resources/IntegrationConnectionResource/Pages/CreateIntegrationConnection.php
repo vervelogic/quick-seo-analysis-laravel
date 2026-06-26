e99365d2f259cdd6b10684f9191837f528e0ed60
vervelogic <abhishek@vervelogic.com>
2026-06-26 07:45:51 +0530

    Add integration create page

---
+++ app/Filament/Resources/IntegrationConnectionResource/Pages/CreateIntegrationConnection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\IntegrationConnectionResource\Pages;
+
+use App\Filament\Resources\IntegrationConnectionResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateIntegrationConnection extends CreateRecord
+{
+    protected static string $resource = IntegrationConnectionResource::class;
+}
