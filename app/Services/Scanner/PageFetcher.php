0734fc72beabe9c44b1a1ede68b091e6646c250c
vervelogic <abhishek@vervelogic.com>
2026-07-04 11:49:48 +0530

    Improve scanner fetch realism

--- app/Services/Scanner/PageFetcher.php
+++ app/Services/Scanner/PageFetcher.php
@@ -203,7 +203,7 @@ class PageFetcher
             'User-Agent' => config('qsa.scan_user_agent'),
             'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
             'Accept-Language' => 'en-US,en;q=0.9',
-            'Accept-Encoding' => 'gzip, deflate',
+            'Accept-Encoding' => 'gzip, deflate, br',
             'Cache-Control' => 'no-cache',
             'Pragma' => 'no-cache',
             'Upgrade-Insecure-Requests' => '1',
