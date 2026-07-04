8790422c1972a2b6036fc2b4133935b818df490e
vervelogic <abhishek@vervelogic.com>
2026-07-04 11:42:42 +0530

    Improve QSA self-scan trust and crawl signals

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Console\Commands\ImportLegacyDotnetCommand;
 use App\Console\Commands\InspectLegacyDotnetCommand;
 use App\Console\Commands\PrepareLegacyAccountsCommand;
 use App\Console\Commands\RepairLegacyAuditTypesCommand;
+use App\Http\Middleware\AddSecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -21,7 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         RepairLegacyAuditTypesCommand::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(AddSecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
