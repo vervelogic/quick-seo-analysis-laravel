a940d20f48291ad8e01d8efbdb6be020feeb3a29
vervelogic <abhishek@vervelogic.com>
2026-07-05 23:57:22 +0530

    Add content redirect list page

---
+++ app/Filament/Resources/ContentRedirectResource/Pages/ListContentRedirects.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ContentRedirectResource\Pages;
+
+use App\Filament\Resources\ContentRedirectResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListContentRedirects extends ListRecords
+{
+    protected static string $resource = ContentRedirectResource::class;
+}
