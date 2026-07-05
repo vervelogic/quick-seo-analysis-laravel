6891539a594edd00660e1a32f9cf2e6eb3caffce
vervelogic <abhishek@vervelogic.com>
2026-07-05 23:56:38 +0530

    Add content import run list page

---
+++ app/Filament/Resources/ContentImportRunResource/Pages/ListContentImportRuns.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ContentImportRunResource\Pages;
+
+use App\Filament\Resources\ContentImportRunResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListContentImportRuns extends ListRecords
+{
+    protected static string $resource = ContentImportRunResource::class;
+}
