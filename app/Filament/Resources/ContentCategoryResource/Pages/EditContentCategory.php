8449b7b9ac22d9898000cbd05a83970ccd259bc2
vervelogic <abhishek@vervelogic.com>
2026-07-05 23:54:57 +0530

    Add content category edit page

---
+++ app/Filament/Resources/ContentCategoryResource/Pages/EditContentCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ContentCategoryResource\Pages;
+
+use App\Filament\Resources\ContentCategoryResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditContentCategory extends EditRecord
+{
+    protected static string $resource = ContentCategoryResource::class;
+}
