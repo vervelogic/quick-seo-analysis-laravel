c7c56ec7febfb3bcd9b6eeecf2bdbfa768484636
vervelogic <abhishek@vervelogic.com>
2026-07-05 23:56:39 +0530

    Add content import run view page

---
+++ app/Filament/Resources/ContentImportRunResource/Pages/ViewContentImportRun.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ContentImportRunResource\Pages;
+
+use App\Filament\Resources\ContentImportRunResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewContentImportRun extends ViewRecord
+{
+    protected static string $resource = ContentImportRunResource::class;
+}
