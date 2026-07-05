52252a6ba60d226e3818e8e5702c38571b5573ed
vervelogic <abhishek@vervelogic.com>
2026-07-05 23:58:17 +0530

    Add content tag list page

---
+++ app/Filament/Resources/ContentTagResource/Pages/ListContentTags.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ContentTagResource\Pages;
+
+use App\Filament\Resources\ContentTagResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListContentTags extends ListRecords
+{
+    protected static string $resource = ContentTagResource::class;
+}
